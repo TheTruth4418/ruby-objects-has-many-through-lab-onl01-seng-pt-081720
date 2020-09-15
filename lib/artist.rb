9f9df71c9ca57608781cf6513f04dce869fc4906
William West <wwest466@gmail.com>
2020-09-15 23:53:34 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << name
+  end
+
+  def new_song(name,genre)
+    song = Song.new 
+    song.name = name
+    song.genre = genre
+  end
+
+  def songs
+    
+  end
+
+  def genres
+
+  end
+
+  def self.all 
+    @@all 
+  end
+end
