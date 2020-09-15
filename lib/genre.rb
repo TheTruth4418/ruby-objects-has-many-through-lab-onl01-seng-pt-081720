fdd6e27ef03012c28736dc63d0a5d3283c412ef0
William West <wwest466@gmail.com>
2020-09-15 23:56:53 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,20 @@
+class Genre
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    self.all << name
+  end
+
+  def songs
+
+  end
+
+  def artists
+
+  end
+
+  def self.all?
+    @@all 
+  end
+end
